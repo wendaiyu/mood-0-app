d0978c2178f5ac936b10cbdd73cf4a22a7d2d402
wendaiyu <3866015403@qq.com>
2026-08-14 20:11:40 +0800

    Update mood.py

--- mood.py
+++ mood.py
@@ -5,7 +5,7 @@ from email.mime.text import MIMEText
 
 st.title("🌤 小小的记录")
 nickname = st.text_input("✨ 今天想叫什么", placeholder="取个可爱的昵称吧")
-where = st.text_input("📍 现在在哪儿", placeholder="上海 / 无锡 / 梦里 / 被窝里/......")
+where = st.text_input("📍 现在在哪儿", placeholder="上海的咖啡馆 / 无锡的火锅店 / 梦里 / 被窝里/......")
 
 #记录今天
 st.subheader("😊 今日心情")
@@ -16,7 +16,7 @@ with col1:
         "😌 平静放松","🥺 想哭","😢 难过","😡 烦躁","😨 焦虑","🥰 被爱到了","🤔 好像都不是"
     ], key="mood_select")
 with col2:
-    mood_desc = st.text_input("💭 为什么会这样呢？（选填）", value="", placeholder="比如：因为因为吃到了好吃的...")
+    mood_desc = st.text_input("💭 为什么会这样呢？（选填）", value="", placeholder="比如吃到了好吃的...")
 final_mood = f"{mood} {mood_desc}".strip()
 note = st.text_area("写点什么吧！对今天的你",height=100)
 
@@ -24,7 +24,7 @@ st.divider()
 st.subheader("明天")
 st.subheader("☁️ 明天天气")
 weather_options=[ "🌞 大晴天 / 阳光明媚","⛅ 多云 / 局部多云","☁️ 阴天 / 灰蒙蒙","🌦️ 阵雨 / 小雨绵绵","🌧️ 大雨 / 暴雨","⛈️ 雷阵雨 / 电闪雷鸣","🌨️ 小雪 / 中雪 / 大雪","❄️ 暴雪 / 冻雨","🌫️ 雾霾 / 大雾弥漫","💨 大风 / 妖风阵阵","🌪️ 台风 / 强对流","🌈 雨后彩虹 / 天气真好","🔥 高温预警 / 热化了","🥶 降温明显 / 冷飕飕","🥵回南天 / 墙壁流泪"]
-weather = st.multiselect("看看明天老天爷给啥脸色：（可以多选~）", weather_options, default=["☁️ 阴天 / 灰蒙蒙"])
+weather = st.multiselect("看看明天老天爷给啥脸色：（可多选~）", weather_options, default=["☁️ 阴天 / 灰蒙蒙"])
 combined_weather_str = "".join(weather)
 if "雨" in combined_weather_str or "雪" in combined_weather_str or "台风" in combined_weather_str:
     tip = "☔️ 记得带伞和外套，路上小心"
@@ -41,9 +41,9 @@ high = st.number_input("最高温（°C）", value=28, step=1)
 
 st.divider()
 # 这里把两个输入框分开命名，避免互相覆盖
-now_do = st.text_area("现在有很想做的事情吗！有很想吃的东西嘛！",height=120)
+now_do = st.text_area("现在最想做什么☺️",height=120)
 st.divider()
-tomorrow_plan = st.text_area("对明天有想说的吗！",height=120)
+tomorrow_plan = st.text_area("明天有任务吗bb🤗?对明天的自己有想说的嘛",height=120)
 
 if st.button("📥 提交"):
     content = f"""
